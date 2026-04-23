1b3ac2319a950661e6d7674bd15a52f1d256de43
narmadaarikatla <narmadaarikatla3@gmail.com>
2026-04-23 20:30:44 +0530

    remove terraform cache files

---
+++ modules/alb/variables.tf
@@ -0,0 +1,17 @@
+variable "name" {
+	type = string
+}
+
+variable "subnet_ids" {
+	type = list(string)
+}
+
+variable "vpc_id" {
+	type = string
+}
+
+variable "tags" {
+	type = map(string)
+	default = {}
+	
+}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "availability_zones" {
+  type    = list(string)
+  default = ["a", "b"]
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
